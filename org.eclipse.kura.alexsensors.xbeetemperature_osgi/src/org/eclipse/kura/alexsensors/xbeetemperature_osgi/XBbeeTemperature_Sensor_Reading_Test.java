8adabfcb7fa8effb39ac04860a8e98029f6800ec
Alex Precosky <precosky@mac.com>
2021-01-03 11:07:03 -0800

    Runs unit tests now with `mvn test`

--- org.eclipse.kura.alexsensors.xbeetemperature_osgi/src/org/eclipse/kura/alexsensors/xbeetemperature_osgi/XBbeeTemperature_Sensor_Reading_Test.java
+++ org.eclipse.kura.alexsensors.xbeetemperature_osgi/src/org/eclipse/kura/alexsensors/xbeetemperature_osgi/XBbeeTemperature_Sensor_Reading_Test.java
@@ -7,33 +7,30 @@ import org.junit.Test;
 
 public class XBbeeTemperature_Sensor_Reading_Test {
 
-	@Test
-	public void testFrom_string_happy_path() {
-		String input = "sensor_data:station=alexfridge&names=temp,battVoltage&values=23.33,4.17&units=C,V";
-		
-		xbeetemperature_sensor_reading reading = new xbeetemperature_sensor_reading();
-	
-		reading.from_string(input);
-		
-		assertEquals(23.33, reading.temperature_c, 0.01);
-		assertEquals(4.17, reading.battery_V, 0.01);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testFrom_string_weird_input() {
-		String input = "this is incorrect input";
-		xbeetemperature_sensor_reading reading = new xbeetemperature_sensor_reading();
-		reading.from_string(input);
-		
-	}
+    @Test
+    public void testFrom_string_happy_path() {
+        String input = "sensor_data:station=alexfridge&names=temp,battVoltage&values=23.33,4.17&units=C,V";
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testFrom_string_missing_voltage() {
-		
-		// Notice there is no voltage in the list of values
-		String input = "sensor_data:station=alexfridge&names=temp,battVoltage&values=23.33,&units=C,V";
-		xbeetemperature_sensor_reading reading = new xbeetemperature_sensor_reading();
-		reading.from_string(input);
-		
-	}
+        xbeetemperature_sensor_reading reading = new xbeetemperature_sensor_reading();
+
+        reading.from_string(input);
+
+        assertEquals(23.33, reading.temperature_c, 0.01);
+        assertEquals(4.17, reading.battery_V, 0.01);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFrom_string_weird_input() {
+        String input = "this is incorrect input";
+        xbeetemperature_sensor_reading reading = new xbeetemperature_sensor_reading();
+        reading.from_string(input);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFrom_string_missing_voltage() {
+        // Notice there is no voltage in the list of values
+        String input = "sensor_data:station=alexfridge&names=temp,battVoltage&values=23.33,&units=C,V";
+        xbeetemperature_sensor_reading reading = new xbeetemperature_sensor_reading();
+        reading.from_string(input);
+    }
 }
